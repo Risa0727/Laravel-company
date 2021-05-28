c1a777bfb0dabc1f271360fc52e927d67f1123f5
risarisarisa <from.aqua.to@gmail.com>
2021-05-28 20:40:57 +1000

    Added css file for designing top page

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
-    return view('top');
-});
+// Route::get('/', function () {
+//     // return view('welcome');
+//     return view('top');
+// });
+Route::get('/', 'TopPageController@show');
