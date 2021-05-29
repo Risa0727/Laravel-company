cd9c4448d76b9a60c637932f318c5e8e80546227
risarisarisa <from.aqua.to@gmail.com>
2021-05-29 19:24:30 +1000

    Arranged about us page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,26 @@
  @include("parts.common", ["title"=>"About - "])
-  @include("parts.header")
-  <main>
-    <h1>About Us</h1>
-    <p>It is {{ $current_time }} now.</p>
-  </main>
-  @include("parts.footer")
+ @include("parts.nav")
+  {{-- @include("parts.header") --}}
+
+{{-- Image showcases --}}
+<section class="showcase">
+  <div class="container-fluid p-0">
+    <div class="row g-0">
+      <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+        <h2>About Us</h2>
+        <p class="lead mb-0">
+          This is ABC Company.<br />
+          Now is {{ $current_time }}. <br />
+
+          <ul>
+            <li>Name: ABC Company</li>
+            <li>Address: LANE COVE WEST​, NEW SOUTH WALES, 2066</li>
+            <li>Company Description: ABC Company is located in LANE COVE WEST, NEW SOUTH WALES, Australia and is part of the Information Technology Services Industry.</li>
+          </ul>
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+@include("parts.footer")
