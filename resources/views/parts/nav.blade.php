4e18dbf47f72c7dfa2b91a2ee1aaf12669aeb381
risarisarisa <from.aqua.to@gmail.com>
2021-07-20 20:50:01 +1000

    updated the navigation design

--- resources/views/parts/nav.blade.php
+++ resources/views/parts/nav.blade.php
@@ -1,8 +1,18 @@
 
 {{-- Navigation --}}
-<nav class="navbar navbar-light bg-light static-top">
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand" href="{{ url('/')}}">HOME</a>
-    <a class="btn btn-primary" href="{{ url('/about')}}">ABOUT</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav4" aria-controls="navbarNav4" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="{{ url('/')}}"><img src="{{ asset('images/logo.png') }}" width="70" height="70"></a>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="{{ url('/')}}">HOME</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ url('/about')}}">ABOUT</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">PORTFOLIO</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">CONTACT</a></li>
+      </ul>
+    </div>
   </div>
 </nav>
